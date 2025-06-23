bfa511fa716daea6f81ac7594037ee189842b104
Mochamad Dwi Febriansyah <iniakunnyaorangjoos@gmail.com>
2025-06-23 22:53:34 +0700

    initial commit

---
+++ src/ping.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+  // summaryTrendStats: ['avg', 'min', 'max', 'p(95)', 'p(99)'],
+};
+
+export default function() {
+  let res = http.get('http://localhost:3000/ping');
+  check(res, { "status is 200": (res) => res.status === 200 });
+  sleep(1);
+}
