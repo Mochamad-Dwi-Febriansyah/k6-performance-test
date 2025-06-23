a18d587c282084f6fb8bda56a1d7f09fb5beb098
Mochamad Dwi Febriansyah <iniakunnyaorangjoos@gmail.com>
2025-06-23 23:30:45 +0700

    feat: stages test

--- src/stages.js
+++ src/stages.js
@@ -1,11 +1,14 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-
+ 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '20s', target: 20 }, // ramp up to 10 users over 30 seconds
+    { duration: '10s', target: 10 }, // stay at 10 users for 1 minute
+    { duration: '20s', target: 0 }, // ramp down to 0 users over 30 seconds
+  ],
   // summaryTrendStats: ['avg', 'min', 'max', 'p(95)', 'p(99)'],
-};
+}
 
 export default function() {
   let res = http.get('http://localhost:3000/ping');
